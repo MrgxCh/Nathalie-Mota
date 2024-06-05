7af94de6a5272f53ac1cab5b2f9caad4ab0ce834
MrgxCh <margauxchantrel2@gmail.com>
2024-06-05 18:11:05 +0200

    single page

--- Nathalie-Mota/single.php
+++ Nathalie-Mota/single.php
@@ -7,31 +7,3 @@
  * @subpackage Nathalie-Mota
  */
 ?>
-
-<?php
-get_header();
-?>
-<div class="main single">
-	<?php
-	if (have_posts()):
-		while (have_posts()):
-			the_post();
-			?>
-			<div class="post">
-				<h1 class="post-title"><?php the_title(); ?> </h1>
-				<div class="post-content">
-					<?php the_content(); ?>
-				</div>
-				<div class="post-comments">
-					<?php
-					comments_template(); ?>
-				</div>
-			</div>
-			<?php
-		endwhile;
-	endif;
-	?>
-</div>
-<?php
-get_sidebar();
-get_footer();
