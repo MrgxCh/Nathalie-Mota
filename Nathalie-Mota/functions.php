33988f7136ff94521ce153c4fa366d17dfddce2b
MrgxCh <margauxchantrel2@gmail.com>
2024-07-01 21:11:36 +0200

    Integration de la zone photos apparentés en single

--- Nathalie-Mota/functions.php
+++ Nathalie-Mota/functions.php
@@ -6,17 +6,21 @@
  * @subpackage Nathalie-Mota
  */
 
-function add_enqueue_scripts()
-{
-    wp_enqueue_style('style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_script('script', get_template_directory_uri() . '/js/script.js', array('jquery'), '1.0', true);
+//Ajout styles
+function enqueue_custom_styles() {
+    wp_enqueue_style('style', get_template_directory_uri() . '/style.css', array(), '1.0', 'all');
+    wp_enqueue_style('single-photo', get_template_directory_uri() . '/assets/CSS/single-photo.css', array(), '1.0', 'all');
 }
-add_action('wp_enqueue_scripts', 'add_enqueue_scripts');
+add_action('wp_enqueue_scripts', 'enqueue_custom_styles');
+
+//Ajout scripts
+function enqueue_custom_scripts() {
+    wp_enqueue_script('script', get_template_directory_uri() . '/js/script.js', array('jquery'), '1.1.1', true);
+}
+add_action('wp_enqueue_scripts', 'enqueue_custom_scripts');
 
 
-wp_enqueue_script('script', get_template_directory_uri() . '/js/script.js');
-
-//Enregistrement des menus header
+//Enregistrement des menus
 function register_my_menus()
 {
     register_nav_menu('main', __('Menu principal', 'text-domain'));
@@ -24,51 +28,48 @@ function register_my_menus()
 }
 add_action('after_setup_theme', 'register_my_menus');
 
-//Custom post 
+function cptui_register_my_cpts_photo() {
 
-function cptui_register_my_cpts_photo()
-{
+	/**
+	 * Post Type: photos.
+	 */
 
-    /**
-     * Post Type: photos.
-     */
+	$labels = [
+		"name" => esc_html__( "photos", "Nathalie-Mota" ),
+		"singular_name" => esc_html__( "photo", "Nathalie-Mota" ),
+	];
 
-    $labels = [
-        "name" => esc_html__("photos", "Nathalie-Mota"),
-        "singular_name" => esc_html__("photo", "Nathalie-Mota"),
-    ];
+	$args = [
+		"label" => esc_html__( "photos", "Nathalie-Mota" ),
+		"labels" => $labels,
+		"description" => "",
+		"public" => true,
+		"publicly_queryable" => true,
+		"show_ui" => true,
+		"show_in_rest" => true,
+		"rest_base" => "",
+		"rest_controller_class" => "WP_REST_Posts_Controller",
+		"rest_namespace" => "wp/v2",
+		"has_archive" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"delete_with_user" => false,
+		"exclude_from_search" => false,
+		"capability_type" => "post",
+		"map_meta_cap" => true,
+		"hierarchical" => false,
+		"can_export" => false,
+		"rewrite" => [ "slug" => "photo", "with_front" => true ],
+		"query_var" => true,
+		"menu_position" => 5,
+		"supports" => [ "title", "editor", "thumbnail", "custom-fields" ],
+		"taxonomies" => [ "categorie", "format" ],
+		"show_in_graphql" => false,
+	];
 
-    $args = [
-        "label" => esc_html__("photos", "Nathalie-Mota"),
-        "labels" => $labels,
-        "description" => "",
-        "public" => true,
-        "publicly_queryable" => true,
-        "show_ui" => true,
-        "show_in_rest" => true,
-        "rest_base" => "",
-        "rest_controller_class" => "WP_REST_Posts_Controller",
-        "rest_namespace" => "wp/v2",
-        "has_archive" => true,
-        "show_in_menu" => true,
-        "show_in_nav_menus" => true,
-        "delete_with_user" => false,
-        "exclude_from_search" => false,
-        "capability_type" => "post",
-        "map_meta_cap" => true,
-        "hierarchical" => false,
-        "can_export" => false,
-        "rewrite" => ["slug" => "photo", "with_front" => true],
-        "query_var" => true,
-        "menu_position" => 5,
-        "supports" => ["title", "editor", "thumbnail", "custom-fields"],
-        "taxonomies" => ["categorie", "format"],
-        "show_in_graphql" => false,
-    ];
-
-    register_post_type("photo", $args);
+	register_post_type( "photo", $args );
 }
 
-add_action('init', 'cptui_register_my_cpts_photo');
+add_action( 'init', 'cptui_register_my_cpts_photo' );
 
 
